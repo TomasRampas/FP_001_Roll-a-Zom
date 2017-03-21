36e84c778dc5f3cc8f89bae4bbc9d7c7571fe3c7
TomasRampas <rampas.tomas@gmail.com>
2017-03-21 10:42:52 +0100

    Game scripts

---
+++ Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public GameManager gameManager;
+    public AudioClip beep;
+    AudioSource source;
+
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+		
+	}
+    
+    void OnTriggerEnter(Collider other)
+    {
+        gameManager.AddPoint();
+        source.PlayOneShot(beep);
+    }
+}
